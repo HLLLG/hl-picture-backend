e7a4bd3ac151a541e2f717ac8226318ef6f4412f
HLLLG <2162803956@qq.com>
2025-12-12 18:10:05 +0800

    init project

---
+++ src/test/java/com/hl/hlpicturebackend/HlPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hl.hlpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
